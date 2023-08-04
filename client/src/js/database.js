e9d9e05d559345029f5b6f4ecb92cb8b741e9dc6
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-08-04 14:16:01 -0400

    Finished implementation

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,38 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) =>{
+  console.error('putDb implemented');
+  // Create a connection to the database and verify that it was created
+  const jadeDb = await openDB('jate', 1);
+  // Create a transaction and store to add the content to the database
+  const tx = jadeDb.transaction('jate', 'readwrite');
+  // Add the content to the database
+  const store = tx.objectStore('jate');
+  // Wait for the request to complete
+  const request = store.put({id:1,  content: content});
+  // Log the result of the request
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb implemented');
+  //Create a connection to the database and verify that it was created
+  const jadeDb = await openDB('jate', 1);
+  // Create a transaction and store to add the content to the database
+  const tx = jadeDb.transaction('jate', 'readonly');
+  // Add the content to the database
+  const store = tx.objectStore('jate');
+  // Wait for the request to complete
+  const request = store.get(1).content;
+  //const request = store.getAll();
+  // Log the result of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,35 @@
 const butInstall = document.getElementById('buttonInstall');
+let deferredPrompt; 
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    deferredPrompt = event;  
+    butInstall.setAttribute('hidden', false);
+    
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  // deferredPrompt is a global variable we've been using in the sample to capture the `beforeinstallevent`
+  deferredPrompt.prompt();
+  // Find out whether the user confirmed the installation or not
+  const { outcome } = await deferredPrompt.userChoice;
+  // The deferredPrompt can only be used once.
+  deferredPrompt = null;
+  // Act on the user's choice
+  if (outcome === 'accepted') {
+    console.log('User accepted the install prompt.');
+  } else if (outcome === 'dismissed') {
+    console.log('User dismissed the install prompt');
+  }
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // prevent default behavior
+   // butInstall.setAttribute('hidden', true);
+    console.log('👍', 'appinstalled', event);
+});
